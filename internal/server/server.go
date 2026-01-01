2556f8c2f1342f38bf10d319549e69a0ebd3e00f
Henock <contacthenockd@gmail.com>
2026-01-01 17:29:45 +0000

    add public listener and handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,10 +1,14 @@
 package server
 
 import (
-	"net"
 	"bufio"
-	"log"
 	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	"net/http"
 
 	"github.com/henockt/cello/internal/config"
 )
@@ -17,7 +21,7 @@ type Server struct {
 func (s *Server) StartChannel() {
 	listener, err := net.Listen("tcp", config.ChannelPort)
 	if err != nil {
-		log.Println("Error starting listener on ", config.ChannelPort)
+		log.Println("Error starting client listener on ", config.ChannelPort)
 	}
 	defer listener.Close()
 	log.Println("Client listener active on", config.ChannelPort)
@@ -73,6 +77,76 @@ func (s *Server) handleClient(conn net.Conn) {
 	}
 }
 
+// public connection listener
+// sends PUB:<RequestId>
 func (s *Server) StartPublic() {
-	log.Println("TODO")
+	listener, err := net.Listen("tcp", config.PublicPort)
+	if err != nil {
+		log.Fatal("Error starting public listener on ", config.PublicPort)
+	}
+	defer listener.Close()
+	log.Println("Public listener active on", config.PublicPort)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Failed to accept public request: ", err)
+			continue
+		}
+		log.Println("Received public request from client")
+		
+		go s.handlePublic(conn)		
+	}
+}
+
+func (s *Server) handlePublic(conn net.Conn) {
+	// defer conn.Close()
+	
+	// send PUB:<requestId> to 'subdomains' conn
+	reader := bufio.NewReader(conn)
+    req, err := http.ReadRequest(reader)
+	if err != nil {
+		log.Println("Error parsing request")
+		return
+	}
+
+	key := strings.Split(req.Host, ".")[0]
+	
+	/*
+		FOR DEVELOPMENT
+	*/
+	key = "myapp"
+	log.Println("key:", key)
+
+	// check for client conn existence
+	clientConn, err := s.cm.get(key)
+	if err != nil {
+		log.Println("no client connection found")
+		sendHTTPResp(conn, 502, "Client not active")
+		conn.Close()
+		return
+	}
+
+	// assign a new unique key for the public request
+	requestId := fmt.Sprintf("%d", time.Now().UnixNano())
+
+	// save (conn, requestId) pair for data listener to use
+	s.cm.add(requestId, conn)
+
+	pub := fmt.Sprintf("%s:%s\n", config.ChannelPublish, requestId)
+	clientConn.Write([]byte(pub))
+
+	go func(id string) {
+		time.Sleep(5 * time.Second)
+		if expiredConn, _ := s.cm.get(id); expiredConn != nil {
+			log.Printf("Request %s timed out waiting for client dial-back", id)
+			sendHTTPResp(expiredConn, 504, "Client agent timed out")
+			expiredConn.Close()
+		}
+	}(requestId)
+}
+
+func sendHTTPResp(conn net.Conn, code int, msg string) {
+	resp := fmt.Sprintf("HTTP/1.1 %d %s\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n%s\n", code, http.StatusText(code), msg)
+	conn.Write([]byte(resp))
 }
